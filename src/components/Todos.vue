7ccf1503d3f2a89d26d82ddb28e9144cc20347ff
Yannik Seitz <yannikseitz@outlook.de>
2020-07-27 16:19:39 +0200

    1

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,23 +1,15 @@
 <template>
-    <div>
-        <div v-bind:key="todo.id" v-for="todo in todos">
-            <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"/>
-        </div>
-    </div>
+  <div>
+      <h3>Todos</h3>
+  </div>
 </template>
 
 <script>
-import TodoItem from './TodoItem.vue'
-
 export default {
-    name: "Todos",
-    components:{
-        TodoItem
-    },
-    props: ["todos"]
+    name: "Todos"
 }
 </script>
 
-<style scoped>
+<style>
 
 </style>
